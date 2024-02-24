ee1166e0a0726a6a916750e62853ae7c13919761
Alwin Brauns <63373755+AlwinBrauns@users.noreply.github.com>
2024-02-24 18:32:07 +0100

    prettier in dev deps and npx prettier on all files (#10)

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <script setup lang="ts">
-import {KeycloakInstance} from "./main.ts";
-import {onMounted, ref} from "vue";
-import {KeycloakInitOptions, KeycloakProfile} from "keycloak-js";
+import { KeycloakInstance } from "./main.ts";
+import { onMounted, ref } from "vue";
+import { KeycloakInitOptions, KeycloakProfile } from "keycloak-js";
 import TheUserHomePage from "./components/TheUserHomePage.vue";
-import {Menu, MenuButton, MenuItem, MenuItems} from "@headlessui/vue";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
 
 const userProfile = ref<KeycloakProfile>();
 
 const loadProfile = async () => {
   userProfile.value = await KeycloakInstance.loadUserProfile();
-}
+};
 
 onMounted(async () => {
   KeycloakInstance.onReady = async (authenticated: boolean | undefined) => {
-    if(!authenticated) return;
+    if (!authenticated) return;
     await loadProfile();
-  }
+  };
   await KeycloakInstance.init(<KeycloakInitOptions>{
-    onLoad: "check-sso"
+    onLoad: "check-sso",
   });
-})
-
+});
 </script>
 
 <template>
-  <div class="container max-w-screen-sm mx-auto p-3 px-10 grid grid-cols-2 items-baseline border-b-4 rounded border-green-400">
+  <div
+    class="container max-w-screen-sm mx-auto p-3 px-10 grid grid-cols-2 items-baseline border-b-4 rounded border-green-400"
+  >
     <div class="text-start">
       <span class="text-2xl">Welcome</span>
     </div>
@@ -32,20 +33,27 @@ onMounted(async () => {
       <Menu as="div" class="relative">
         <MenuButton>Account</MenuButton>
         <transition
-            enter-active-class="transition duration-100 ease-out"
-            enter-from-class="transform scale-95 opacity-0"
-            enter-to-class="transform scale-100 opacity-100"
-            leave-active-class="transition duration-75 ease-in"
-            leave-from-class="transform scale-100 opacity-100"
-            leave-to-class="transform scale-95 opacity-0"
+          enter-active-class="transition duration-100 ease-out"
+          enter-from-class="transform scale-95 opacity-0"
+          enter-to-class="transform scale-100 opacity-100"
+          leave-active-class="transition duration-75 ease-in"
+          leave-from-class="transform scale-100 opacity-100"
+          leave-to-class="transform scale-95 opacity-0"
         >
-          <MenuItems as="ul" class="absolute right-0 p-5 mt-1.5 w-max min-w-32 flex flex-col rounded shadow bg-white">
+          <MenuItems
+            as="ul"
+            class="absolute right-0 p-5 mt-1.5 w-max min-w-32 flex flex-col rounded shadow bg-white"
+          >
             <MenuItem>
-              <a href="#" @click="KeycloakInstance.logout" v-if="userProfile">Logout</a>
+              <a href="#" @click="KeycloakInstance.logout" v-if="userProfile"
+                >Logout</a
+              >
               <a href="#" @click="KeycloakInstance.login" v-else>Login</a>
             </MenuItem>
             <MenuItem v-if="userProfile">
-              <a href="#" @click="KeycloakInstance.accountManagement">Your Account</a>
+              <a href="#" @click="KeycloakInstance.accountManagement"
+                >Your Account</a
+              >
             </MenuItem>
           </MenuItems>
         </transition>
@@ -56,4 +64,3 @@ onMounted(async () => {
     <TheUserHomePage :user-profile="userProfile" />
   </template>
 </template>
-
--- src/components/TheUserHomePage.vue
+++ src/components/TheUserHomePage.vue
@@ -1,31 +1,38 @@
 <script setup lang="ts">
-import {KeycloakInstance} from "../main.ts";
-import {KeycloakProfile} from "keycloak-js";
-import {onMounted, ref} from "vue";
+import { KeycloakInstance } from "../main.ts";
+import { KeycloakProfile } from "keycloak-js";
+import { onMounted, ref } from "vue";
 import ollamaAdapter from "../backend-connection/ollamaAdapter.ts";
 
 const ping = ref();
+const userTextInput = ref();
 
 defineProps<{
-  userProfile: KeycloakProfile
-}>()
+  userProfile: KeycloakProfile;
+}>();
 
 onMounted(async () => {
-  const {data} = await ollamaAdapter.get('api/ollama/ping');
+  const { data } = await ollamaAdapter.get("api/ollama/ping");
   ping.value = data;
-})
+});
 </script>
 
 <template>
-  <p>
-    Hallo {{ userProfile.firstName }}!
-  </p>
+  <p>Hallo {{ userProfile.firstName }}!</p>
+  <textarea
+    name="userTextInput"
+    id="userTextInput"
+    cols="30"
+    rows="1"
+    class="p-2"
+    v-model="userTextInput"
+  ></textarea>
   <p>
     Server Ping:
-    {{ping}}
+    {{ ping }}
     User:
-    {{KeycloakInstance.hasRealmRole('user')}}
+    {{ KeycloakInstance.hasRealmRole("user") }}
     Maintainer:
-    {{KeycloakInstance.hasRealmRole('maintainer')}}
+    {{ KeycloakInstance.hasRealmRole("maintainer") }}
   </p>
 </template>
