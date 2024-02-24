dc3d6b345284a364c42c907fd39e14f89dfdf2ad
Alwin Brauns <63373755+AlwinBrauns@users.noreply.github.com>
2024-02-24 12:12:48 +0100

    Release/v0.1.0 (#9)
    * first test (#7)
    
    Co-authored-by: Alwin Brauns <alwinbrauns@MacBook-Pro-von-Alwin.local>
    
    * pong
    
    ---------
    
    Co-authored-by: Alwin Brauns <alwinbrauns@MacBook-Pro-von-Alwin.local>

--- src/components/TheUserHomePage.vue
+++ src/components/TheUserHomePage.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import {KeycloakInstance} from "../main.ts";
 import {KeycloakProfile} from "keycloak-js";
+import {onMounted, ref} from "vue";
+import ollamaAdapter from "../backend-connection/ollamaAdapter.ts";
+
+const ping = ref();
 
 defineProps<{
   userProfile: KeycloakProfile
 }>()
+
+onMounted(async () => {
+  const {data} = await ollamaAdapter.get('api/ollama/ping');
+  ping.value = data;
+})
 </script>
 
 <template>
@@ -12,6 +21,8 @@ defineProps<{
     Hallo {{ userProfile.firstName }}!
   </p>
   <p>
+    Server Ping:
+    {{ping}}
     User:
     {{KeycloakInstance.hasRealmRole('user')}}
     Maintainer:
