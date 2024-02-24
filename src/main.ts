dbada269214477dfb9532e936d5c0e81628d7f0b
Alwin Brauns <musicproductionbrauns@gmail.com>
2024-02-24 18:30:35 +0100

    prettier in dev deps and npx prettier on all files

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-    url: "https://auth.bybrauns.com/",
-    realm: "bybrauns",
-    clientId: window.origin.includes('dev') || window.origin.includes('localhost') ? 'dev-bybrauns' : 'bybrauns'
+  url: "https://auth.bybrauns.com/",
+  realm: "bybrauns",
+  clientId:
+    window.origin.includes("dev") || window.origin.includes("localhost")
+      ? "dev-bybrauns"
+      : "bybrauns",
 });
 export const KeycloakInstance = keycloak;
-createApp(App).mount('#app')
+createApp(App).mount("#app");
